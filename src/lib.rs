a0f25bf90b34d06adf26e63ac5e71ee600f44e9d
Timmy Xiao <34635512+tzx@users.noreply.github.com>
2022-06-04 19:05:31 -0400

    impl Default for Chip8

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,36 @@
 const MEM_SIZE: usize = 4096;
 const SCREEN_HEIGHT: usize = 32;
 const SCREEN_WIDTH: usize = 64;
+const NUM_V_REGS: usize = 16;
+const STACK_SIZE: usize = 16;
+const PC_START: u16 = 0x200;
 
+// can't derive default for const generics:
+// https://github.com/rust-lang/rust/pull/60466#discussion_r280989938
 struct Chip8 {
     memory: [u8; MEM_SIZE],
-    v_regs: [u8; 16],        // general purpose registers
-    dt: u8,                  // delay timer
-    st: u8,                  // sound timer
-    pc: u16,                 // program counter
-    sp: u8,                  // stack pointer
-    stack: [u16; 16],
+    v_regs: [u8; NUM_V_REGS], // general purpose registers
+    dt: u8,                   // delay timer
+    st: u8,                   // sound timer
+    pc: u16,                  // program counter
+    sp: u8,                   // stack pointer
+    stack: [u16; STACK_SIZE],
     keyboard: u16,
     display: [[u8; SCREEN_WIDTH]; SCREEN_HEIGHT],
+}
+
+impl Default for Chip8 {
+    fn default() -> Self {
+        Chip8 {
+            memory: [0; MEM_SIZE],
+            v_regs: [0; NUM_V_REGS],
+            dt: 0,
+            st: 0,
+            pc: PC_START,
+            sp: 0,
+            stack: [0; STACK_SIZE],
+            keyboard: 0,
+            display: [[0; SCREEN_WIDTH]; SCREEN_HEIGHT],
+        }
+    }
 }
