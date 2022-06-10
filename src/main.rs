9d44363d0ef98257a060d0b3ce67f01839d4f4ff
Timmy Xiao <34635512+tzx@users.noreply.github.com>
2022-06-10 19:53:13 -0400

    Screen drawing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,30 @@
 use std::{env, fs::File, io::Read};
 
 use chip8::{SCREEN_HEIGHT, SCREEN_WIDTH, Chip8};
-use sdl2::event::Event;
+use rand::RngCore;
+use sdl2::{event::Event, render::Canvas, pixels::Color, rect::Rect, video::Window};
 
 
 const SCALE: u32 = 20;
 const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
 const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
+
+fn draw_screen<R: RngCore>(chip8: &Chip8<R>, canvas: &mut Canvas<Window>) {
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+
+    let display = chip8.get_display();
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+    for (r, row) in display.iter().enumerate() {
+        for (c, col) in row.iter().enumerate() {
+            if *col == 1 {
+                let rect = Rect::new((r as u32 * SCALE) as i32, (c as u32 * SCALE) as i32, SCALE, SCALE);
+                canvas.fill_rect(rect).unwrap();
+            }
+        }
+    }
+    canvas.present();
+}
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -22,7 +40,6 @@ fn main() {
     let mut buffer = Vec::new();
     rom.read_to_end(&mut buffer).expect("unable to read file to buffer");
     chip8.load_rom_data(&buffer);
-
 
 
     let sdl_context = sdl2::init().unwrap();
@@ -48,6 +65,8 @@ fn main() {
                 }
                 _ => {}
             }
+            chip8.tick();
+            draw_screen(&chip8, &mut canvas);
         }
     }
 
